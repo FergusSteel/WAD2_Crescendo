ce85239e9cec9289471a903b14f83da110aa2421
FergusSteel <fergus.k.steel@gmail.com>
2022-03-03 17:58:05 +0000

    Linked crescendo_app urls to main urls.py

--- crescendo/urls.py
+++ crescendo/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from crescendo_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('crescendo/', include(crescendo_app.urls))
 ]
