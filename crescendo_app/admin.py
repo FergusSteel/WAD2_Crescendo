6b35da7a1786e61182e9914bb18972f735c9b8aa
Michal Wozniak <2554268W@student.gla.ac.uk>
2022-03-04 14:17:46 +0000

    Modified the admin interface

--- crescendo_app/admin.py
+++ crescendo_app/admin.py
@@ -1,21 +1,26 @@
+
 from django.contrib import admin 
 from crescendo_app.models import UserProfile,Genre,Song,SongComment,Playlist,PlaylistComment
 
-# Register your models here.
+# Register your models here. 
+ 
+class SongAdmin(admin.ModelAdmin): 
+    list_display = ('name','artist',)
+  
+class PlaylistAdmin(admin.ModelAdmin): 
+    list_display = ('name','author',) 
+     
+class GenreAdmin(admin.ModelAdmin): 
+    list_display = ('name',) 
+     
+class CommentAdmin(admin.ModelAdmin):  
+    list_display = ('author','rating',)
+     
   
 admin.site.register(UserProfile) 
-admin.site.register(Genre) 
-admin.site.register(Song) 
-admin.site.register(SongComment) 
-admin.site.register(Playlist) 
-admin.site.register(PlaylistComment)   
+admin.site.register(Genre, GenreAdmin) 
+admin.site.register(Song, SongAdmin) 
+admin.site.register(SongComment , CommentAdmin) 
+admin.site.register(Playlist , PlaylistAdmin) 
+admin.site.register(PlaylistComment , CommentAdmin)   
  
-
-# class GenreAdmin(admin.ModelAdmin): 
-#     list_display = ('name') 
-
-# class SongAdmin(admin.ModelAdmin): 
-#     list_display  = ('name','artist','genre') 
-
-# class PlaylistAdmin(admin.ModelAdmin): 
-#     list_display  = ('name','genre')  
