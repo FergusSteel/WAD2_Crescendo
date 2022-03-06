747e8295270318433ce1f677206b449bd0336141
Michal Wozniak <2554268w@student.gla.ac.uk>
2022-03-06 23:27:11 +0000

    Minor changes to the population script and added default path to file fields in models

--- crescendo_app/models.py
+++ crescendo_app/models.py
@@ -10,26 +10,7 @@ class UserProfile(models.Model):
 
     numberOfComments = models.IntegerField(default=0)
     numberOfProfileViews = models.IntegerField(default=0)
-    image = models.ImageField(upload_to="profile_images", blank=True)
-
-    def __str__(self):
-        return self.user.username
-
-
-class Genre(models.Model):
-    name = models.CharField(unique=True, max_length=30)
-
-    def __str__(self):
-        return self.name
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    numberOfComments = models.IntegerField(default=0)
-    numberOfProfileViews = models.IntegerField(default=0)
-    image = models.ImageField(upload_to=(STATIC_DIR + "/profile_images")[1:],
-                              default=(STATIC_DIR + '/profile_images/image.jpg')[1:])
+    image = models.ImageField(upload_to="profile_images", default = 'profile_images/default.jpg')
 
     def __str__(self):
         return self.user.username
@@ -49,8 +30,7 @@ class Playlist(models.Model):
     views = models.IntegerField(default=0)
     numberOfComments = models.IntegerField(default=0)
     description = models.CharField(max_length=300)
-    image = models.ImageField(upload_to=(STATIC_DIR + "/playlist_images")[1:],
-                              default=(STATIC_DIR + "/playlist_images/image.jpg")[1:])
+    image = models.ImageField(upload_to="playlist_images" , default = 'playlist_images/default.jpg')
 
     def __str__(self):
         return self.name
@@ -64,11 +44,12 @@ class Comment(models.Model):
 
 class Song(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    genre = models.ManyToManyField(Genre)
+    genre = models.ManyToManyField(Genre) 
+    playlist = models.ManyToManyField(Playlist)
     name = models.CharField(max_length=30)
     artist = models.CharField(max_length=300)
     numberOfComments = models.IntegerField()
-    image = models.ImageField(upload_to="song_images", blank=True)
+    image = models.ImageField(upload_to="song_images", default = 'song_images/default.jpg')
     lyrics = models.CharField(max_length=1000)
     actualSong = models.FileField(upload_to="music_files")
 
@@ -88,23 +69,3 @@ class PlaylistComment(Comment):
 
     def __str__(self):
         return "Comment about a playlist from " + self.author.user + " of rating" + self.rating
-
-
-class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.CharField(max_length=300)
-    rating = models.IntegerField()
-
-
-class SongComment(Comment):
-    song = models.ForeignKey(Song, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return "Comment about a song from " + self.author + " of rating" + self.rating
-
-
-class PlaylistComment(Comment):
-    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return "Comment about a playlist from " + self.author + " of rating" + self.rating
