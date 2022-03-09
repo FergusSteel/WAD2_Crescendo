33e4a05fefb105d97d6981a46cf1fe8f123c92fc
Max Johnston <2295595j@student.gla.ac.uk>
2022-03-09 20:54:39 +0000

    Added Media to urls

--- crescendo/urls.py
+++ crescendo/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from crescendo_app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('crescendo/', include('crescendo_app.urls')),
     path('accounts/', include('registration.backends.simple.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
