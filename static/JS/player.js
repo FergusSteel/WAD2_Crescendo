2d32d6d61f2d12e5a9f271c3f8f4c01580c5b01a
FergusSteel <fergus.k.steel@gmail.com>
2022-03-11 00:44:30 +0000

    Added Comments, Fixed CSS and added control icons in the worst way ever

--- static/JS/player.js
+++ static/JS/player.js
@@ -1,9 +1,12 @@
 function playAudio() {
   var audio = document.getElementById("audiofile");
+  var play = document.getElementById("topimage");
   console.log("image clicked")
   if (audio.paused) {
     audio.play();
+    play.src = document.getElementById("var1").value;
   } else {
     audio.pause();
+    play.src = document.getElementById("var2").value;
   }
 }
