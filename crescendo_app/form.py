23e75c162b7cc2dbc4372103b9b687d1b7417a55
Angelo Singh <2470584s@student.gla.ac.uk>
2022-03-16 17:43:17 +0000

    Working add_playlist

--- crescendo_app/form.py
+++ crescendo_app/form.py
@@ -5,9 +5,9 @@ from crescendo_app.models import UserProfile, Song, Playlist, Genre
 
 
 class PlaylistForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="Please enter the crescendo_app name.")
+    
+    name = forms.CharField(max_length=128, help_text="Please enter the playlist name.")
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    comments = forms.CharField(max_length=128, help_text="Please enter comment...")
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     class Meta:
